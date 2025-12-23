4c7461c73155d707843f9d8bdd554d26be601d15
Kapil Karki <lotuskapil31@gmail.com>
2025-12-24 05:26:31 +0800

    removing commented codes

--- app/Http/Controllers/ProductSyncController.php
+++ app/Http/Controllers/ProductSyncController.php
@@ -11,37 +11,7 @@ class ProductSyncController extends Controller
         private ProductSyncService $syncService
     ) {}
 
-    // public function sync()
-    // {
-    //     $shop = 'assessment-app.myshopify.com';
-    //     $token = Shop::where('shop', $shop)->value('access_token');
-    //     if (!$token) {
-    //         abort(403, 'Shop not installed');
-    //     }
-    //     $response = Http::withHeaders([
-    //         'X-Shopify-Access-Token' => $token,
-    //     ])->get("https://{$shop}/admin/api/2025-10/products.json");
-
-    //     $products = $response->json('products');
-
-    //     foreach ($products as $item) {
-    //         Product::updateOrCreate(
-    //             [
-    //                 'shop' => $shop,
-    //                 'shopify_product_id' => $item['id'],
-    //             ],
-    //             [
-    //                 'title' => $item['title'],
-    //                 'status' => $item['status'],
-    //             ]
-    //         );
-    //     }
-
-    //     return response()->json([
-    //         'synced' => count($products),
-    //     ]);
-    // }
-
+   
     public function sync(Request $request)
     {
        

dc55f4642dbc3c95e819f0a3d2107ad2ed89ebfc
Kapil Karki <lotuskapil31@gmail.com>
2025-12-25 03:25:12 +0800

    fix: dashboard api based on shop :)

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -13,9 +13,9 @@ class DashboardController extends Controller
     {
         $shop_id = auth()->user()->id;
         return response()->json([
-            'total_products' => Product::where('shop_id',$shop_id)::count(),
-            'total_collections'=> Collection::where('shop_id',$shop_id)::count(),
-            'last_sync' => SyncLogger::where('shop_id',$shop_id)::latest('last_sync')->value('last_sync')?->diffForHumans(),
+            'total_products' => Product::where('shop_id',$shop_id)->count(),
+            'total_collections'=> Collection::where('shop_id',$shop_id)->count(),
+            'last_sync' => SyncLogger::where('shop_id',$shop_id)->latest('last_sync')->value('last_sync')?->diffForHumans(),
         ]);
     }
 
